6eb602edc793bfdfbb49e45491e6ec41c67b646b
Anthoni <anthonifelipi@gmail.com>
2022-07-14 13:43:17 -0300

    create: custom class AppError and middlewares appError.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import "express-async-errors"
+import appErrorMiddleware from "./middlewares/appError.middlewares";
 
 const app = express();
 app.use(express.json());
 
-// app.use("users", userRoutes);
+// app.use("users", userRoutes); 
 // app.use("products", productsRoutes);
 // app.use("companies", companiesRoutes);
 // app.use("orders", ordersRoutes);
 // app.use("categories", categoriesRoutes);
+
+app.use(appErrorMiddleware);
 
 export default app;
