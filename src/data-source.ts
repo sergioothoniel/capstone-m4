995a5b244bbd2b419b46624c0dc76000a6edbf59
sergioothoniel <sergioothoniel@hotmail.com>
2022-07-15 10:54:07 -0300

    update: atulização para fazer o deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -15,11 +15,15 @@ const appDataSource =
       })
     : new DataSource({
         type: "postgres",
-        host: process.env.POSTGRES_HOST,
-        port: 5432,
-        username: process.env.POSTGRES_USER,
-        password: process.env.POSTGRES_PASSWORD,
-        database: process.env.POSTGRES_DB,
+        // host: process.env.POSTGRES_HOST,
+        // port: 5432,
+        // username: process.env.POSTGRES_USER,
+        // password: process.env.POSTGRES_PASSWORD,
+        // database: process.env.POSTGRES_DB,
+        url: process.env.DB_URL,
+        synchronize: false,
+        logging: true,
+        ssl: {rejectUnauthorized: false},
         entities: ["src/entities/*.ts"],
         migrations: ["src/migrations/*.ts"],
       });
